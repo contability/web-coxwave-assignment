8bc204548bb37c8c1d0b023a1da638ac18928b11
contability <shouldbnewb@gmail.com>
2025-10-02 20:08:12 +0900

    (fix) useEventList 훅의 매개변수 순서 변경 및 쿼리 옵션 추가

--- src/lib/hooks/event.ts
+++ src/lib/hooks/event.ts
@@ -1,6 +1,10 @@
 import { listEvents } from '@buf/alignai_frontend-challenge-datetz.connectrpc_query-es/event/v1/event-EventService_connectquery';
 import { useQuery } from '@connectrpc/connect-query';
 
-export const useEventList = (projectId: string, pageSize: number, pageToken: string, filter?: string) => {
-  return useQuery(listEvents, { projectId, pageSize, pageToken, filter });
+export const useEventList = (pageSize: number, pageToken: string, projectId?: string, filter?: string) => {
+  return useQuery(
+    listEvents,
+    { projectId, pageSize, pageToken, filter },
+    { refetchOnWindowFocus: false, enabled: !!projectId },
+  );
 };
